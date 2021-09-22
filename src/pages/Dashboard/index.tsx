8bfa5d9562045922b7ff5dd84da04cf9cd940954
wellymaya <welly.maya.s@gmail.com>
2021-09-22 04:16:03 -0300

    adding styled-components

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,20 @@
 import React from "react";
-import './styles.ts'
+import {Title, Form} from './styles'
+import logoImg from '../../assets/logo.svg';
+
 
 const Dashboard: React.FC = () => {
-    return <h1>Explore github Repositories</h1>;
+    return (
+    <>
+        <img src ={logoImg} alt="Github Explorer" />
+        <Title>Explore github Repositories</Title>
+
+        <Form >
+            <input placeholder="Type Here the repository name: "></input>
+            <button>Find</button>
+        </Form>
+    </>
+    );
 
 }
 
